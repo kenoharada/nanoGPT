e19021c550fbb10e49fd8ecc417b7fb45a8ff0f3
kenoharada <keno.lasalle.kagoshima@gmail.com>
2023-05-14 19:09:07 +0900

    modified

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -10,9 +10,12 @@ from datasets import load_dataset # huggingface datasets
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
 num_proc = 8
+# __file__ is the path of the current script.
+# os.path.dirname() gets the directory that the script is in.
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
-dataset = load_dataset("openwebtext")
+dataset = load_dataset("openwebtext", cache_dir=script_dir)
 
 # owt by default only contains the 'train' split, so create a test split
 split_dataset = dataset["train"].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
